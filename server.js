ae3adc840e15a280ec9d5f5db32f495444d15216
Brandon Maxwell <brandon.l.maxwell@gmail.com>
2021-04-12 19:39:24 -0500

    added server.js

---
+++ server.js
@@ -0,0 +1,45 @@
+// Dependencies
+const express = require('express');
+const path = require('path');
+// Sets up the Express App
+const app = express();
+const PORT = 3000;
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Reservation data
+const reservations = [
+  {
+    routeName: 'yoda',
+    name: 'Yoda',
+    phoneNumber: 'Jedi Master',
+    email: 900,
+    id: 2000,
+  },
+];
+// Waitlist data
+const waitlist= [
+  {
+    routeName: 'yoda',
+    name: 'Yoda',
+    phoneNumber: 'Jedi Master',
+    email: 900,
+    id: 2000,
+  },
+];
+// Routes
+// Basic route that sends the user first to the AJAX Page
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'home.html')));
+app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, 'reserve.html')));
+app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, 'tables.html')));
+// Displays all reservations
+// Create New reservations - takes in JSON input
+app.post('/api/reservations', (req, res) => {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  const newReservation = req.body;
+});
+// Starts the server to begin listening
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+
+// add clear table
